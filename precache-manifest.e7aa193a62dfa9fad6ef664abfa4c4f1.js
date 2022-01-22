4efb14e04b5fd4f373a52b0a5bc45ef1820bd79e
Nelson Alvarez <nalvare84523@universidadean.edu.co>
2022-01-22 12:09:03 -0500

    Updates

---
+++ precache-manifest.e7aa193a62dfa9fad6ef664abfa4c4f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28358040344c87aca973c7a2e33d28c",
+    "url": "/NALVARE84523/react-movies/index.html"
+  },
+  {
+    "revision": "011ca14ba2a952fd0ceb",
+    "url": "/NALVARE84523/react-movies/static/css/main.bdfc2a5b.chunk.css"
+  },
+  {
+    "revision": "18333ef539d7ef772c4c",
+    "url": "/NALVARE84523/react-movies/static/js/2.95c8142f.chunk.js"
+  },
+  {
+    "revision": "011ca14ba2a952fd0ceb",
+    "url": "/NALVARE84523/react-movies/static/js/main.e22552de.chunk.js"
+  },
+  {
+    "revision": "85c4f56081a34045b3ea",
+    "url": "/NALVARE84523/react-movies/static/js/runtime~main.53173771.js"
+  }
+]);
